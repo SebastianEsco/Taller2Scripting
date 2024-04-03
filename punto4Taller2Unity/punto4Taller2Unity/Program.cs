194b5cc843ae1a64ebdc064c5872a5deb6c2ebeb
santiago <santiagocardenasgomez@gmail.com>
2024-04-03 11:28:13 -0500

    implementacion Singleton Unity

---
+++ punto4Taller2Unity/punto4Taller2Unity/Program.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class GameManager: MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            // evita que el objeto se destruya al cargar una nueva escena
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            // Si ya existe una instancia destruye la nueva instancia
+            Destroy(gameObject);
+        }
+    }
+}
+    
+
+
