f2d7ddee5a53bf99ce623788afe6bf77ed19bce1
santiago <santiagocardenasgomez@gmail.com>
2024-04-03 11:31:38 -0500

    punto4Taller2CorgiEngine

---
+++ Punto4Taller2CorgiEngine/Punto4Taller2CorgiEngine/Program.cs
@@ -0,0 +1,20 @@
+﻿public class LevelManager : Singleton<LevelManager>
+{
+    public string[] LevelNames;
+
+    public void LoadLevel(int index)
+    {
+        StartCoroutine(LoadLevelAsync(LevelNames[index]));
+    }
+
+    private IEnumerator LoadLevelAsync(string levelName)
+    {
+        // Mostrar pantalla de carga
+
+        yield return SceneManager.LoadSceneAsync(levelName);
+
+        // Ocultar pantalla de carga
+    }
+}
+
+
